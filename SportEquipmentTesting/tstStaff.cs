23634f175d4bdd59a2704fd31b3e64aebbe443b2
Grzegorz Fordon <P2500674@my365.dmu.ac.uk>
2020-02-10 17:08:33 +0000

    tstStaff and clsStaff have ben created.

---
+++ SportEquipmentTesting/tstStaff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SportEquipmentTesting
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff guy = new clsStaff();
+            Assert.IsNotNull(guy);
+        }
+    }
+}
