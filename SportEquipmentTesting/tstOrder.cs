d4b56b03781f957858b666712915c4dc60dad7e7
Aston Elderkin <P2414525@my365.dmu.ac.uk>
2020-02-17 17:54:58 +0000

    testOrderStarted

--- SportEquipmentTesting/tstOrder.cs
+++ SportEquipmentTesting/tstOrder.cs
@@ -4,16 +4,23 @@ using SportEquipmentClasses;
 
 namespace SportEquipmentTesting
 {
-    [TestClass]
-    public class tstOrder
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            //create an instance of the class we want to create
-            clsOrder AnOrder = new clsOrder();
-            //test to see that it exists
-            Assert.IsNotNull(AnOrder);
+	[TestClass]
+	public class tstOrder
+	{
+		
+		
+            [TestMethod]
+            public void InstanceOK()
+            {
+                //create an instance of the class we want to create
+                clsOrder AOrder = new clsOrder();
+                //test to see that it exists
+                Assert.IsNotNull(AOrder);
+            }
+
+
+
+
         }
     }
-}
+
