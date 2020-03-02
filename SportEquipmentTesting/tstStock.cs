b599871ef88ec9915559277f64730f04c20b20f4
Elliot Forde <P2416030@my365.dmu.ac.uk>
2020-03-02 17:40:11 +0000

    Added testing and class for stock management

---
+++ SportEquipmentTesting/tstStock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportEquipmentClasses;
+
+namespace SportEquipmentTesting
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock AnStock = new clsStock();
+            Assert.IsNotNull(AnStock);
+        }
+    }
+}
