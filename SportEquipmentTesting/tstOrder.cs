185417c4a60750585ed3c3a6509b7da64eb1a34e
Aston Elderkin <P2414525@my365.dmu.ac.uk>
2020-03-02 17:52:25 +0000

    update tstOrder

--- SportEquipmentTesting/tstOrder.cs
+++ SportEquipmentTesting/tstOrder.cs
@@ -16,9 +16,76 @@ namespace SportEquipmentTesting
                 Assert.IsNotNull(AOrder);
             }
 
+        //OrderNumber
+            [TestMethod]
+            public void OrderNumberOK()
+            {
+                clsOrder AOrder = new clsOrder();
+                Int32 TestData = 1;
+                AOrder.OrderNumber = TestData;
+                Assert.AreEqual(AOrder.OrderNumber, TestData);
+            }
 
+        //OrderDescription
+            [TestMethod]
+            public void OrderDescriptionOK()
+            {
+                clsOrder AOrder = new clsOrder();
+                string TestData = "123Test";
+                AOrder.OrderDescription = TestData;
+                Assert.AreEqual(AOrder.OrderDescription, TestData);
+            }
 
+        //OrderDatePlaced
+            [TestMethod]
+            public void OrderDatePlacedOK()
+            {
+                clsOrder AOrder = new clsOrder();
+                DateTime TestData = DateTime.Now.Date;
+                AOrder.OrderDatePlaced = TestData;
+                Assert.AreEqual(AOrder.OrderDatePlaced, TestData);
+            }
 
-        }
+        //OrderCompleted
+            [TestMethod]
+            public void OrderNumberOK()
+            {
+                clsOrder AOrder = new clsOrder();
+                Boolean TestData = true;
+                AOrder.OrderCompleted = TestData;
+                Assert.AreEqual(AOrder.OrderCompleted, TestData);
+            }
+
+        //OrderPrice
+            [TestMethod]
+            public void OrderNumberOK()
+            {
+                clsOrder AOrder = new clsOrder();
+                float TestData = 6.9;
+                AOrder.OrderPrice = TestData;
+                Assert.AreEqual(AOrder.OrderPrice, TestData);
+            }
+
+        //CustomerID
+            [TestMethod]
+            public void OrderNumberOK()
+            {
+                clsOrder AOrder = new clsOrder();
+                Int32 TestData = 2;
+                AOrder.CustomerID = TestData;
+                Assert.AreEqual(AOrder.CustomerID, TestData);
+            }
+
+        //StaffID
+            [TestMethod]
+            public void OrderNumberOK()
+            {
+                clsOrder AOrder = new clsOrder();
+                Int32 TestData = 3;
+                AOrder.OrderPrice = TestData;
+                Assert.AreEqual(AOrder.OrderPrice, TestData);
+            }
+
+    }
     }
 
---
+++ SportEquipmentTesting/tstOrderLine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SportEquipmentTesting
+{
+    [TestClass]
+    public class tstOrderLine
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            //test to see that it exists
+            Assert.IsNotNull(AnOrderLine);
+        }
+
+        //OrderLineNumber
+        [TestMethod]
+        public void OrderNumberOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Int32 TestData = 1;
+            AnOrderLine.OrderLineNumber = TestData;
+            Assert.AreEqual(AnOrderLine.OrderLineNumber, TestData);
+        }
+
+        //OrderNumber
+        [TestMethod]
+        public void OrderNumberOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Int32 TestData = 2;
+            AnOrderLine.OrderNumber = TestData;
+            Assert.AreEqual(AnOrderLine.OrdereNumber, TestData);
+        }
+
+        //ProductID
+        [TestMethod]
+        public void OrderNumberOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Int32 TestData = 3;
+            AnOrderLine.ProductID = TestData;
+            Assert.AreEqual(AnOrderLine.ProductID, TestData);
+        }
+
+    }
+}
