4293a62f575ffc3f676c28c1fffcb7a33b1acf80
Elliot Forde <P2416030@my365.dmu.ac.uk>
2020-03-20 17:45:27 +0000

    TestingUpdate

--- SportEquipmentTesting/tstStock.cs
+++ SportEquipmentTesting/tstStock.cs
@@ -21,11 +21,32 @@ namespace SportEquipmentTesting
             AnProduct.ProductId = TestData;
             Assert.AreEqual(AnProduct.ProductId, TestData);
         }
-
-        //ProductDetails
-        //Price
-        //DateAcquired
-        //InStock
-    }
-
+        public void ProductDetailsOK()
+        {
+            clsStock AnDetail = new clsStock();
+            String TestData = 50;
+            AnDetail.ProductId = TestData;
+            Assert.AreEqual(AnDetail.ProductId, TestData);
+        }
+        public void PriceOK()
+        {
+            clsStock AnPrice = new clsStock();
+            Decimal TestData = 9.99;
+            AnPrice.ProductId = TestData;
+            Assert.AreEqual(AnPrice.ProductId, TestData);
+        }
+        public void DateAcquiredOK()
+        {
+            clsStock Aquired = new clsStock();
+            DateTime TestData = DateTime.Now.Date;
+            Aquired.ProductId = TestData;
+            Assert.AreEqual(Aquired.ProductId, TestData);
+        }
+        public void InStockOK()
+        {
+            clsStock AnStock = new clsStock();
+            Boolean TestData = true;
+            AnStock.ProductId = TestData;
+            Assert.AreEqual(AnStock.ProductId, TestData);
+        }
 }
