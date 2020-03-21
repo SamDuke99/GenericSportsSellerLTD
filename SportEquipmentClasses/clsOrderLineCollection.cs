9f9fd212ca20f82a0921ff562dbcabd441ba4dfd
Aston Elderkin <P2414525@my365.dmu.ac.uk>
2020-03-21 16:03:34 +0000

    minor tweaks to clsOrderLine

--- SportEquipmentClasses/clsOrderLineCollection.cs
+++ SportEquipmentClasses/clsOrderLineCollection.cs
@@ -8,6 +8,105 @@ namespace SportEquipmentClasses
 {
     public class clsOrderLineCollection
     {
-        public List<clsOrderLine> OrderLineList { get; set; }
+
+        List<clsOrderLine> mOrderLineList = new List<clsOrderLine>();
+        clsOrderLine mThisOrderLine = new clsOrderLine();
+
+        public List<clsOrderLine> OrderLineList
+        {
+            get
+            {
+                return mOrderLineList;
+            }
+            set
+            {
+                mOrderLineList = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return mOrderLineList.Count;
+            }
+            set
+            {
+
+            }
+        }
+
+        public clsOrderLine ThisOrderLine
+        {
+            get
+            {
+                return mThisOrderLine;
+            }
+            set
+            {
+                mThisOrderLine = value;
+            }
+        }
+
+        //constructor
+        public clsOrderLineCollection()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblOrderLine_SelectAll");
+            PopulateArray(DB);
+        }
+
+        public int Add()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@OrderNumber", mThisOrderLine.OrderNumber);
+            DB.AddParameter("@ProductID", mThisOrderLine.ProductID);
+
+            return DB.Execute("sproc_tblOrderLine_Insert");
+        }
+
+        public void Delete()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@OrderLineNumber", mThisOrderLine.OrderLineNumber);
+            DB.Execute("sproc_tblOrderLine_Delete");
+        }
+
+        public void Update()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@OrderLineNumber", mThisOrderLine.OrderLineNumber);
+            DB.AddParameter("@OrderNumber", mThisOrderLine.OrderNumber);
+            DB.AddParameter("@ProductID", mThisOrderLine.ProductID);
+            DB.Execute("sproc_tblOrder_Update");
+        }
+
+        public void ReportByOrderNumber(string OrderNumber)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@OrderNumber", OrderNumber);
+            DB.Execute("sproc_tblOrderLine_FilterByOrderNumber");
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+
+            RecordCount = DB.Count;
+
+            mOrderLineList = new List<clsOrderLine>();
+
+            while (Index < RecordCount)
+            {
+                clsOrderLine AnOrderLine = new clsOrderLine();
+                AnOrderLine.OrderLineNumber = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderLineNumber"]);
+                AnOrderLine.OrderNumber = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderNumber"]);
+                AnOrderLine.ProductID = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductID"]);
+                mOrderLineList.Add(AnOrderLine);
+                Index++;
+            }
+        }
     }
 }
