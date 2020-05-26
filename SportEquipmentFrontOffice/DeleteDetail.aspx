f7ae8b6b1526e0d0f2bc13a9d51aec54acfd388a
Elliot Forde <P2416030@my365.dmu.ac.uk>
2020-05-26 11:41:30 +0100

    Update DeleteDetail

--- SportEquipmentFrontOffice/DeleteDetail.aspx
+++ SportEquipmentFrontOffice/DeleteDetail.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DeleteDetail.aspx.cs" Inherits="Default2" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DeleteDetail.aspx.cs" Inherits="" %>
 
 <!DOCTYPE html>
 
--- SportEquipmentFrontOffice/DeleteDetail.aspx.cs
+++ SportEquipmentFrontOffice/DeleteDetail.aspx.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SportEquipmentClasses;
 
-public partial class Default2 : System.Web.UI.Page
+public partial class DeleteStock : System.Web.UI.Page
 {
+    Int32 ProductId;
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        ProductId = Convert.ToInt32(Session["ProductId"]);
+    }
+    protected void btnYes_Click(object sender, EventArgs e)
+    {
+        clsStockCollection stockList = new clsStockCollection();
+        stockList.ThisProduct.Find(ProductId);
+        stockList.Delete();
+        Response.Redirect("AnDetailList.aspx");
+    }
 
+    protected void btnNo_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("AnDetailList.aspx");
     }
 }
